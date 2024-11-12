c93ebd5b914d31f0b69150f8aea26b623c5a9af9
Faisal Falalu Sa'adu <faisalfalalu7@gmail.com>
2024-11-12 13:30:46 +0100

    Initial commit

---
+++ app/Http/Controllers/FormTeacherRemarkController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormTeacherRemarkController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/ProprietorRemarkController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProprietorRemarkController extends Controller
+{
+    //
+}
